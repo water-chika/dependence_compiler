7888f5c50b1c0357aa43b535ca554444157a9810
kangxchi@amd.com <kangxchi@amd.com>
2024-03-26 06:39:46 -0700

    Refine codes.

--- dependence_compiler.hpp
+++ dependence_compiler.hpp
@@ -5,15 +5,62 @@
 #include <ranges>
 #include <set>
 #include <string>
+#include <vector>
+#include <cassert>
 
-void generate_init_all_func(
-    std::multimap<std::string, std::string> dependences,
-    std::map<std::string, std::string> init_funcs)
+template <bool all_node_needed = false, bool no_belong_to = false>
+struct node_depend_and_belong_to_graph
 {
-    std::map<std::string, bool> are_inited{};
-    for (auto &[k, v] : init_funcs)
+    std::set<std::string> nodes;
+    std::multimap<std::string, std::string> depend_relations;
+    std::map<std::string, std::string> belong_to_relations;
+    std::set<std::string> needed_nodes;
+};
+template <>
+struct node_depend_and_belong_to_graph<true>
+{
+    std::set<std::string> nodes;
+    std::multimap<std::string, std::string> depend_relations;
+    std::map<std::string, std::string> belong_to_relations;
+};
+template <>
+struct node_depend_and_belong_to_graph<true, true>
+{
+    std::set<std::string> nodes;
+    std::multimap<std::string, std::string> depend_relations;
+};
+
+bool is_nodes_sorted(node_depend_and_belong_to_graph<true, true> graph, std::vector<std::string> nodes)
+{
+    bool result = true;
+    std::set<std::string> previous_nodes{};
+    for (auto &node : nodes)
     {
-        are_inited.emplace(k, false);
+        result = graph.nodes.contains(node);
+        if (result == false)
+            break;
+        for (auto [ite, end] = graph.depend_relations.equal_range(node);
+             ite != end;
+             ite++)
+        {
+            auto [n, depend_node] = *ite;
+            result = previous_nodes.contains(depend_node);
+            if (result == false)
+                break;
+        }
+        previous_nodes.emplace(node);
+    }
+    return result;
+}
+
+std::vector<std::string> generate_sorted_nodes(node_depend_and_belong_to_graph<true, true> graph)
+{
+    std::multimap<std::string, std::string> dependences = graph.depend_relations;
+    std::vector<std::string> sorted_nodes{};
+    std::map<std::string, bool> are_inited{};
+    for (auto &node : graph.nodes)
+    {
+        are_inited.emplace(node, false);
     }
     for (auto &[k, v] : dependences)
     {
@@ -34,89 +81,22 @@ void generate_init_all_func(
                     std::all_of(b, e, [&are_inited](auto &k_v)
                                 { return are_inited[k_v.second]; }))
                 {
-                    if (init_funcs.contains(k))
-                    {
-                        std::cout << "//" << k << std::endl;
-                        std::cout << init_funcs[k] << std::endl;
-                    }
+                    sorted_nodes.emplace_back(k);
                     are_inited[k] = true;
                 }
             }
         }
     }
-}
 
-void generate_needed_nodes_init_func(
-    std::multimap<std::string, std::string> dependences,
-    std::map<std::string, std::string> init_funcs,
-    std::set<std::string> needed_nodes)
-{
-    std::map<std::string, bool> are_inited{};
-    for (auto &[k, v] : init_funcs)
-    {
-        are_inited.emplace(k, false);
-    }
-    for (auto &[k, v] : dependences)
-    {
-        are_inited.emplace(k, false);
-        are_inited.emplace(v, false);
-    }
-    while (std::ranges::any_of(needed_nodes, [&are_inited](auto &node)
-                               { return false == are_inited[node]; }))
-    {
-        for (auto &node : needed_nodes)
-        {
-            if (false == are_inited[node])
-            {
-                auto [b, e] = dependences.equal_range(node);
-                std::for_each(b, e, [&needed_nodes](auto &k_v) mutable
-                              { needed_nodes.emplace(k_v.second); });
-                if (auto [b, e] = dependences.equal_range(node);
-                    std::all_of(b, e, [&are_inited](auto &k_v)
-                                { return are_inited[k_v.second]; }))
-                {
-                    if (init_funcs.contains(node))
-                    {
-                        std::cout << init_funcs[node] << std::endl;
-                    }
-                    are_inited[node] = true;
-                }
-            }
-        }
-    }
-}
+    assert(is_nodes_sorted(graph, sorted_nodes));
 
-template <bool all_node_needed = false, bool no_belong_to = false>
-struct node_depend_and_belong_to_graph
-{
-    std::multimap<std::string, std::string> depend_relations;
-    std::map<std::string, std::string> nodes;
-    std::map<std::string, std::string> belong_to_relations;
-    std::set<std::string> needed_nodes;
-};
-template <>
-struct node_depend_and_belong_to_graph<true>
-{
-    std::multimap<std::string, std::string> depend_relations;
-    std::map<std::string, std::string> nodes;
-    std::map<std::string, std::string> belong_to_relations;
-};
-template <>
-struct node_depend_and_belong_to_graph<true, true>
-{
-    std::multimap<std::string, std::string> depend_relations;
-    std::map<std::string, std::string> nodes;
-};
-
-void generate_sorted_nodes(node_depend_and_belong_to_graph<true, true> graph)
-{
-    generate_init_all_func(graph.depend_relations, graph.nodes);
+    return sorted_nodes;
 }
 
 std::string get_ultimate_node(
     std::map<std::string, std::string> &ultimate_node_map,
     std::map<std::string, std::string> &belong_to_relations,
-    const std::string node)
+    std::string node)
 {
     if (!ultimate_node_map.contains(node))
     {
@@ -138,62 +118,99 @@ std::string get_ultimate_node(
     return ultimate_node_map[node];
 }
 
-void generate_sorted_nodes(node_depend_and_belong_to_graph<true> graph)
+bool is_nodes_sorted(node_depend_and_belong_to_graph<true> graph, std::vector<std::string> nodes) {
+    return false;
+}
+
+std::vector<std::string> generate_sorted_nodes(node_depend_and_belong_to_graph<true> graph)
 {
+    std::vector<std::string> sorted_nodes{};
+
     std::map<std::string, node_depend_and_belong_to_graph<true>> ultimate_node_graphs{};
+    node_depend_and_belong_to_graph<true, true> ultimate_node_graph_graph{};
     std::map<std::string, std::string> ultimate_node_map{};
-    for (auto [node, init] : graph.nodes)
+
+    for (auto node : graph.nodes)
     {
         auto ultimate_node = get_ultimate_node(ultimate_node_map, graph.belong_to_relations, node);
         if (!ultimate_node_graphs.contains(ultimate_node))
         {
             ultimate_node_graphs.emplace(ultimate_node, node_depend_and_belong_to_graph<true>{});
+            ultimate_node_graph_graph.nodes.emplace(ultimate_node);
         }
-        auto &ultimate_node_graph = ultimate_node_graphs[ultimate_node];
-        ultimate_node_graph.nodes.emplace(node, init);
-        if (graph.belong_to_relations.contains(node)
-            && graph.belong_to_relations[node] != ultimate_node)
+        if (ultimate_node != node)
         {
-            ultimate_node_graph
-                .belong_to_relations
-                .emplace(
-                    node,
-                    graph.belong_to_relations[node]);
-        }
-        for (auto [ite, end] = graph.depend_relations.equal_range(node); ite != end; ite++)
-        {
-            auto &[n, depend_node] = *ite;
-            ultimate_node_graph.depend_relations.emplace(node, depend_node);
+            auto &ultimate_node_graph = ultimate_node_graphs[ultimate_node];
+            ultimate_node_graph.nodes.emplace(node);
+            if (graph.belong_to_relations.contains(node) && graph.belong_to_relations[node] != ultimate_node)
+            {
+                ultimate_node_graph
+                    .belong_to_relations
+                    .emplace(
+                        node,
+                        graph.belong_to_relations[node]);
+            }
+            for (auto [ite, end] = graph.depend_relations.equal_range(node); ite != end; ite++)
+            {
+                auto &[n, depend_node] = *ite;
+                auto depend_node_ultimate_node = get_ultimate_node(ultimate_node_map, graph.belong_to_relations, depend_node);
+                if (ultimate_node == depend_node_ultimate_node)
+                {
+                    ultimate_node_graph.depend_relations.emplace(node, depend_node);
+                }
+                else
+                {
+                    ultimate_node_graph_graph.depend_relations.emplace(ultimate_node, depend_node_ultimate_node);
+                }
+            }
         }
     }
 
-    for (auto &[ultimate_node, ultimate_node_graph] : ultimate_node_graphs)
+    auto sorted_ultimate_nodes = generate_sorted_nodes(ultimate_node_graph_graph);
+
+    for (auto &ultimate_node : sorted_ultimate_nodes)
     {
+        sorted_nodes.emplace_back(ultimate_node);
+        auto &ultimate_node_graph = ultimate_node_graphs[ultimate_node];
+        std::vector<std::string> graph_sorted_nodes{};
         if (!ultimate_node_graph.belong_to_relations.empty())
         {
-            generate_sorted_nodes(ultimate_node_graph);
+            graph_sorted_nodes = generate_sorted_nodes(ultimate_node_graph);
         }
         else
         {
             node_depend_and_belong_to_graph<true, true> graph{};
-            graph.depend_relations = ultimate_node_graph.depend_relations;
             graph.nodes = ultimate_node_graph.nodes;
-            generate_sorted_nodes(std::move(graph));
+            graph.depend_relations = ultimate_node_graph.depend_relations;
+            graph_sorted_nodes = generate_sorted_nodes(std::move(graph));
+        }
+        for (auto &node : graph_sorted_nodes)
+        {
+            sorted_nodes.emplace_back(node);
         }
     }
+
+    return sorted_nodes;
 }
 
-void generate_sorted_nodes(node_depend_and_belong_to_graph<> graph)
+bool is_nodes_sorted(node_depend_and_belong_to_graph<> graph, std::vector<std::string> nodes)
+{
+    bool result = true;
+    return result;
+}
+
+std::vector<std::string> generate_sorted_nodes(
+    node_depend_and_belong_to_graph<> graph)
 {
     node_depend_and_belong_to_graph<true> refined_graph{};
-    std::set<std::string> needed_nodes{};
-    for (std::set<std::string> next_needed_nodes{std::move(graph.needed_nodes)};
+    for (std::set<std::string> next_needed_nodes{graph.needed_nodes};
          false == next_needed_nodes.empty();)
     {
         std::set<std::string> next_next_needed_nodes{};
         std::ranges::for_each(next_needed_nodes,
                               [&graph, &refined_graph, &next_next_needed_nodes](auto &node)
                               {
+                                  refined_graph.nodes.emplace(node);
                                   auto [begin, end] = graph.depend_relations.equal_range(node);
                                   for (auto ite = begin; ite != end; ite++)
                                   {
@@ -208,9 +225,10 @@ void generate_sorted_nodes(node_depend_and_belong_to_graph<> graph)
                                           graph.belong_to_relations[node]);
                                       next_next_needed_nodes.emplace(graph.belong_to_relations[node]);
                                   }
-                                  refined_graph.nodes.emplace(node, graph.nodes[node]);
                               });
-        next_needed_nodes = std::move(next_next_needed_nodes);
+        next_needed_nodes = next_next_needed_nodes;
     }
-    generate_sorted_nodes(refined_graph);
+    std::vector<std::string> sorted_nodes = generate_sorted_nodes(refined_graph);
+    assert(is_nodes_sorted(graph, sorted_nodes));
+    return sorted_nodes;
 }
--- main.cpp
+++ main.cpp
@@ -22,10 +22,27 @@ int main()
 {
     yyparse();
     node_depend_and_belong_to_graph<> graph{};
-    graph.nodes = init_funcs;
+    for (auto& [k,v]: dependences) {
+        graph.nodes.emplace(k);
+        graph.nodes.emplace(v);
+    }
+    for (auto& [k,v]: init_funcs) {
+        graph.nodes.emplace(k);
+    }
+    for (auto& node : needed_nodes) {
+        graph.nodes.emplace(node);
+    }
+    for (auto& [k,v] : belong_to_relations) {
+        graph.nodes.emplace(k);
+        graph.nodes.emplace(v);
+    }
     graph.needed_nodes = needed_nodes;
     graph.belong_to_relations = belong_to_relations;
     graph.depend_relations = dependences;
-    generate_sorted_nodes(graph);
+    auto sorted_nodes = generate_sorted_nodes(graph);
+    for (auto& node : sorted_nodes) {
+        std::cout << "//" << node << std::endl;
+        std::cout << init_funcs[node] << std::endl;
+    }
     return 0;
 }
